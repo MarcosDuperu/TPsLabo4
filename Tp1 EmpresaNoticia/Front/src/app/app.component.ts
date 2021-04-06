b25dd0866061af51d14bcc598ccc58f69af6a3c2
Juanma <juanma.contre91@gmail.com>
2021-04-06 19:42:35 -0300

    Inicio Maquetacion corrección de errores

--- Tp1 EmpresaNoticia/Front/src/app/app.component.ts
+++ Tp1 EmpresaNoticia/Front/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css', '../assets/css/camera.css', '../assets/css/google-map.css','../assets/css/search.css']
+  styleUrls: ['./app.component.css','../assets/css/google-map.css','../assets/css/search.css']
 })
 export class AppComponent {
   title = 'TP1-laboratorio4';
