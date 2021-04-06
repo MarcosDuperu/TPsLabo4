ed90f5d5529d626bb54b21794b8e126aecd07f52
Marcos Duperu <marcos.duperu@alumnos.frm.utn.edu.ar>
2021-04-06 12:06:53 -0300

    cree archivo TestA.js y index.html sigo tratando de hacer funcion de fechas

--- Tp3 TypeScript/modeloA/Vehiculo.ts
+++ Tp3 TypeScript/modeloA/Vehiculo.ts
@@ -1,41 +1,44 @@
 import { HojaRuta } from './HojaRuta';
-export class Vehiculo{
-    patente:String;
-    marca:String;
-    modelo:String;
+import { Detalle } from './Detalles';
+export class Vehiculo {
+    patente: String;
+    marca: String;
+    modelo: String;
     hojaRuta: HojaRuta;
+    detalles: Detalle;
+    calcularTotalKilometrosRecorridos(fechaDesde: Date, fechaHasta: Date) {
+     let contadorD = 0;
+        //minutos:
+     let mS = this.detalles.minutoSalida = fechaDesde.getMinutes();
+     let mE =  this.detalles.minutoRegreso = fechaHasta.getMinutes();
 
-    calcularTotalKilometrosRecorridos ( fechaDesde:Date, fechaHasta:Date){
-    
-       fechaDesde.setDate(12/08/2020);//ingreso de fechas
-       fechaHasta.setDate(13/08/2020);//ingreso de fechas
-let dias = 0; //ingresar el usuario los dias
+      //horas:
+     let hS = this.detalles.horaSalida = fechaDesde.getHours();
+     let hE =this.detalles.horaRegreso = fechaHasta.getHours();
 
-let kmDesde; //ingreso de km desde
-let kmHasta; // ingreso de km hasta
+        
+     
 
-    
-
-
-
-
-
-    /*for(fechaDesde; fechaDesde <= fechaHasta; dias ++){
-
-        fechaDesde.setDate(fechaDesde.getDate() + dias);
-
-    }
-
-    if(fechaDesde){
-     kmDesde = this.hojaRuta.detalle.setkmSalida;
-    }
-    if(fechaHasta){
-      kmHasta = this.hojaRuta.detalle.setkmRegreso;
-    }
+        
         
 
-     return this.hojaRuta.calcularTotalKilometro();
+        /*
+function calcularDiasAusencia(fechaIni, fechaFin) {
+  var diaEnMils = 1000 * 60 * 60 * 24,
+      desde = new Date(fechaIni.substr(0, 10)),
+      hasta = new Date(fechaFin.substr(0, 10)),
+      diff = hasta.getTime() - desde.getTime() + diaEnMils;// +1 incluir el dia de ini
+  return diff / diaEnMils;
+  var fecha1 = moment("2016-09-30 07:30:00", "YYYY-MM-DD HH:mm:ss");
+var fecha2 = moment("2016-10-03 07:30:00", "YYYY-MM-DD HH:mm:ss");
 
-    }*/
+var diff = fecha2.diff(fecha1, 'd'); // Diff in days
+console.log(diff);
+
+var diff = fecha2.diff(fecha1, 'h'); // Diff in hours
+console.log(diff);
 }
+    */
+
+    }
 }
