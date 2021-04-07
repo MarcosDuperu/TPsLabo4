9760e07b613f98dca29578c92d420a8c824f8fd9
Marcos Duperu <marcos.duperu@alumnos.frm.utn.edu.ar>
2021-04-07 12:11:52 -0300

    termino de metodos falta prueba TestA

---
+++ Tp3 TypeScript/modeloA/TestA.ts
@@ -0,0 +1,14 @@
+import { Vehiculo } from './Vehiculo';
+import { HojaRuta } from './HojaRuta';
+import { Detalle } from './Detalles';
+class TestA{
+
+    //creacion de instancias:
+    //vehiculo
+   v1: Vehiculo = new Vehiculo('asd-465','Fiat','Palio');
+        
+    hJR: HojaRuta = new HojaRuta();
+
+    
+    
+}
