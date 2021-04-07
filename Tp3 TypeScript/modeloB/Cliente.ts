11b480e4df9fe787e1c49df5eb910f5e86c94ee4
Marcos Duperu <marcos.duperu@alumnos.frm.utn.edu.ar>
2021-04-07 13:30:03 -0300

    metodo de modeloB realizados falta prueba testB

--- Tp3 TypeScript/modeloB/Cliente.ts
+++ Tp3 TypeScript/modeloB/Cliente.ts
@@ -5,4 +5,36 @@ export class Cliente {
     razonSocial:String;
     cuit: number;
     factura: Factura[];
+
+totalFacturadoXTipoPago(tipoPago:String){
+    let i:number;
+tipoPago.toUpperCase;
+    for(i = 0 ; i<=this.factura.length ; i++){
+        if(this.factura[i].tipoPago == tipoPago){
+            if('E'){
+                console.log("Usted realizo un pago en Efectivo, monto a pagar $",this.factura[i].totalFinal);
+            }
+            if('TD'){
+                console.log("Usted realizo un pago con Tarjeta de Debito, monto a pagar $",this.factura[i].totalFinal);
+            }
+            if('TC'){
+                console.log("Usted realizo un pago con Tarjeta de Credito, monto a pagar $",this.factura[i].totalFinal);
+            }
+            if('CC'){
+                console.log("Usted realizo un pago a Cuenta Corriente, monto a pagar $",this.factura[i].totalFinal);
+            }
+            if('TR'){
+                console.log("Usted realizo un pago mediante Trasnferencia, monto a pagar $",this.factura[i].totalFinal);
+            }
+        }
+        if(tipoPago == null){
+            this.factura[i].totalFinal;
+
+        }
+    }
+    
+
+}
+
+
 }
