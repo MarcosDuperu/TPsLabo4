223377b89ee8a7de5af5fe44ba2431cb5107809c
Enzo1s <enziitoomartin@gmail.com>
2021-04-20 03:00:29 -0300

    Enrutamiento y formulario sin funcion

---
+++ tp4node/dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const rutas_1 = __importDefault(require("./rutas"));
+const app = express_1.default();
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use(rutas_1.default);
+app.listen(3000, () => {
+    console.log(`Servidor en puerto 3000`, 3000);
+});
--- tp4node/src/index.ts
+++ tp4node/src/index.ts
@@ -1,6 +1,13 @@
 import express from "express";
+import rutas from "./rutas";
 const app = express();
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
+
+app.use(rutas);
+
+app.listen(3000, () => {
+  console.log(`Servidor en puerto 3000`, 3000);
+});
